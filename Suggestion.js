4c54840be0d3d6e106e2b252e72db9348e231f0e
Hyunsoo Lee <wrkbrs@gmail.com>
2022-05-01 04:48:09 +0900

    Add selectedIndex를 이용해 강조 처리하기 - Suggestion 구현하기

--- Suggestion.js
+++ Suggestion.js
@@ -19,12 +19,12 @@ export default function Suggestion({ $target, initialState }) {
   };
 
   this.render = () => {
-    const { items = [] } = this.state;
+    const { items = [], selectedIndex } = this.state;
 
     if (items.length > 0) {
-      console.log("items.length > 0...");
-      console.log(items.map((item) => item));
-      console.log(items.map((item) => item).join(""));
+      // console.log("items.length > 0...");
+      // console.log(items.map((item) => item));
+      // console.log(items.map((item) => item).join(""));
 
       // 문제설명에서 "각 태그의 class 이름을 바꾸거나 스타일을 임의로 추가하면 안 됩니다." 라고 했는데
       // 스타일을 바꾸면 안되는 건줄 알았는데 추가가 안된다는 것이었지 기존의 스타일은 바꿔도 된다는 말이었다.
@@ -34,7 +34,12 @@ export default function Suggestion({ $target, initialState }) {
       <ul>
         ${
           items
-            .map((item, index) => `<li data-index="${index}">${item}</li>`)
+            .map(
+              (item, index) =>
+                `<li class="${
+                  index === selectedIndex ? "Suggestion__item--selected" : ""
+                }" data-index="${index}">${item}</li>`
+            )
             .join("") // map() 함수는 Array ["<li>Javascript</li>", "<li>Java</li>"] 배열을 반환되기 때문에 배열의 요소를 빈 문자열로 구분해서 문자열로 합친다.
         }
       </ul>
