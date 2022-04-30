945c16d5a54967595b3e375ea66e08595f9af38d
Hyunsoo Lee <wrkbrs@gmail.com>
2022-04-30 22:26:06 +0900

    Add 렌더링 먼저 해보기 - Suggestion 구현하기

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import SearchInput from "./SearchInput.js";
 import { fetchLanguages } from "./api.js";
+import Suggestion from "./Suggestion.js";
 
 export default function App({ $target }) {
   this.state = {
@@ -8,15 +9,42 @@ export default function App({ $target }) {
   };
 
   this.setState = (nextState) => {
-    // TODO: 구현해야함
+    console.log("App... setState... before this.state: ", this.state);
+    console.log("App... setState... nextState: ", nextState);
+
+    this.state = {
+      ...this.state,
+      ...nextState,
+    };
+    suggestion.setState({
+      items: this.state.fetchedLanguages,
+    });
+
+    console.log("App... setState... after this.state: ", this.state);
   };
 
   const searchInput = new SearchInput({
     $target,
     initialState: "",
     onChange: async (keyword) => {
-      const languages = await fetchLanguages(keyword);
-      console.log(languages);
+      console.log("keyword: ", keyword);
+      if (keyword.length === 0) {
+        this.setState({
+          fetchedLanguages: [],
+        });
+      } else {
+        const languages = await fetchLanguages(keyword);
+        this.setState({
+          fetchedLanguages: languages,
+        });
+      }
+    },
+  });
+
+  const suggestion = new Suggestion({
+    $target,
+    initialState: {
+      items: [],
     },
   });
 }
---
+++ Suggestion.js
@@ -0,0 +1,46 @@
+export default function Suggestion({ $target, initialState }) {
+  this.$element = document.createElement("div");
+  this.$element.className = "Suggestion";
+  $target.appendChild(this.$element);
+
+  // Suggestion으로 전달되는 인수는 다음과 같다
+  // new Suggestion({ $target, initialState: { items: [] } })
+  this.state = initialState;
+
+  this.setState = (nextState) => {
+    console.log("Seggestion... setState... nextState: ", nextState);
+    this.state = nextState;
+    this.render();
+  };
+
+  this.render = () => {
+    const { items = [] } = this.state;
+
+    if (items.length > 0) {
+      console.log("items.length > 0...");
+      console.log(items.map((item) => item));
+      console.log(items.map((item) => item).join(""));
+
+      // 문제설명에서 "각 태그의 class 이름을 바꾸거나 스타일을 임의로 추가하면 안 됩니다." 라고 했는데
+      // 스타일을 바꾸면 안되는 건줄 알았는데 추가가 안된다는 것이었지 기존의 스타일은 바꿔도 된다는 말이었다.
+      // 어떤 태그든 display 속성은 존재하니까 문제해설에서는 이 속성을 활용했다.
+      this.$element.style.display = "block";
+      this.$element.innerHTML = `
+      <ul>
+        ${
+          items
+            .map((item, index) => `<li data-index="${index}">${item}</li>`)
+            .join("") // map() 함수는 Array ["<li>Javascript</li>", "<li>Java</li>"] 배열을 반환되기 때문에 배열의 요소를 빈 문자열로 구분해서 문자열로 합친다.
+        }
+      </ul>
+      `;
+      // 참고 Array.prototype.join():
+      // https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/join
+    } else {
+      this.$element.style.display = "none";
+      this.$element.innerHTML = "";
+    }
+  };
+
+  this.render();
+}
